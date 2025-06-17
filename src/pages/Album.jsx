f728320081cb65bc2f815ce0806ba4a2691c21fa
Gangadhar-mudhiraj <raghava9080@gmail.com>
2025-06-17 20:18:46 +0530

    18img add

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import pic1 from "../assets/pic1.png";
-import pic2 from "../assets/pic2.png";
-import pic3 from "../assets/pic3.png";
-import pic4 from "../assets/pic4.png";
-import pic5 from "../assets/pic5.png";
-import pic6 from "../assets/pic6.png";
-import pic7 from "../assets/pic7.png";
+import pic1 from "../assets/1.jpeg";
+import pic2 from "../assets/2.png";
+import pic3 from "../assets/3.jpeg";
+import pic4 from "../assets/4.jpeg";
+import pic5 from "../assets/5.jpeg";
+import pic6 from "../assets/6.jpeg";
+import pic7 from "../assets/7.png";
+import pic8 from "../assets/8.jpeg";
+import pic9 from "../assets/9.png";
+import pic10 from "../assets/10.jpeg";
+import pic11 from "../assets/11.jpeg";
+import pic12 from "../assets/12.jpeg";
+import pic13 from "../assets/13.jpeg";
+import pic14 from "../assets/14.jpeg";
+import pic15 from "../assets/15.jpeg";
+import pic16 from "../assets/16.jpeg";
+import pic17 from "../assets/17.jpeg";
+import pic18 from "../assets/18.jpeg";
+
 import Title from "../utils/Title";
 
 const Album = () => {
-    const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7];
+    const images = [
+        pic1, pic2, pic3, pic4, pic5, pic6,
+        pic7, pic8, pic9, pic10, pic11, pic12,
+        pic13, pic14, pic15, pic16, pic17, pic18
+    ];
 
     return (
         <div className="min-h-screen bg-sky-100 p-8"> {/* Darker sky blue background */}
